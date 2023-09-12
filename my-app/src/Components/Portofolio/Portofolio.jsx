2f5c6891c0b04e21657d572f17f2efc2c9786662
Abduvaliev <140178160+Abdumannof0@users.noreply.github.com>
2023-09-12 17:45:00 +0300

    #fix #dananz #service

--- my-app/src/Components/Portofolio/Portofolio.jsx
+++ my-app/src/Components/Portofolio/Portofolio.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Portofolio.scss"
+import axios from 'axios'
 
 export default function Portofolio() {
-  return (
-    <div>Portofolio</div>
+  const[product, setProduct] = useState([])
+  useEffect(() => {
+    axios.get(`http://localhost:9000/products`)
+    .then(res => setProduct(res.data))
+    .cath(err => console.log(err))
+
+  }, [])
+   return (
+    <>
+    <div className="row">
+      {product.slice(21, 22).map(product => {
+        return(
+          <React.Fragment key={product.id}>
+            <img src={product.env.PUBLIC_URL + "/images/" + product.image} alt="" />
+
+          </React.Fragment>
+        )
+      })}
+    </div>
+    
+    </>
   )
 }
